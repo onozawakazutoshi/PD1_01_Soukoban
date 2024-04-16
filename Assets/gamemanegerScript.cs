99973ca36459891a2ac325f7f0b19efbac7b4f26
onozawakazutoshi <146906422+onozawakazutoshi@users.noreply.github.com>
2024-04-16 17:08:49 +0900

    2次元未完成

--- Assets/gamemanegerScript.cs
+++ Assets/gamemanegerScript.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class gamemanegerScript : MonoBehaviour
 {
-    int[] map;
+    int[,] map;
     string debugText = "";
     // Start is called before the first frame update
     void Start()
     {
-        map = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0 };
-        for(int i = 0; i < map.Length; i++)
-        {
-            debugText += map[i].ToString() + ",";
-        }
-        Debug.Log(debugText);
+        map = new int[,] {
+            {0, 0, 0, 0, 0 },
+            {0, 0, 1, 0, 0 },
+            {0, 0, 0, 0, 0 },
+
+            };
+        PrintArray();
     }
 
     // Update is called once per frame
@@ -22,51 +23,103 @@ public class gamemanegerScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            int playerIndex = -1;
-            for(int i = 0; i < map.Length; i++)
-            {
-                if (map[i] == 1)
-                {
-                    playerIndex = i;
-                    break;
-                }
-            }
-            if (playerIndex < map.Length - 1)
-            {
-                map[playerIndex + 1] = 1;
-                map[playerIndex] = 0;
-            }
+            int[] playerIndex = {-1,-1 };
+            
+            playerIndex = GetPlayerIndex();
+            
+            MoveNumber(1, playerIndex[1], playerIndex[1] + 1, playerIndex[0],1);
 
-            string debugText = "";
-            for (int i = 0; i < map.Length; i++)
-            {
-                debugText += map[i].ToString() + ",";
-            }
-            Debug.Log(debugText);
+            PrintArray();
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            int playerIndex = -1;
-            for (int i = 0; i < map.Length; i++)
+            int[] playerIndex = { -1, -1 };
+            playerIndex = GetPlayerIndex();
+
+            MoveNumber(1, playerIndex[1], playerIndex[1] - 1, playerIndex[0],1);
+
+            PrintArray();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            int[] playerIndex = { -1, -1 };
+
+            playerIndex = GetPlayerIndex();
+
+            MoveNumber(1, playerIndex[0], playerIndex[0] + 1, playerIndex[1],0);
+
+            PrintArray();
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            int[] playerIndex = { -1, -1 };
+            playerIndex = GetPlayerIndex();
+
+            MoveNumber(1, playerIndex[0], playerIndex[0] - 1, playerIndex[1],0);
+
+            PrintArray();
+        }
+    }
+    void PrintArray()
+    {
+        string debugText = "";
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
             {
-                if (map[i] == 1)
+               debugText += map[y,x].ToString() + ",";
+            }
+            debugText += "\n";
+        }
+        Debug.Log(debugText);
+    }
+
+    int[] GetPlayerIndex()
+    {
+        int[] ans = { -1, -1 };
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                if (map[y,x] == 1)
                 {
-                    playerIndex = i;
-                    break;
+                    ans[0] = y;
+                    ans[1] = x;
+                    return ans;
                 }
             }
-            if (playerIndex > 0)
-            {
-                map[playerIndex - 1] = 1;
-                map[playerIndex] = 0;
-            }
-
-            string debugText = "";
-            for (int i = 0; i < map.Length; i++)
-            {
-                debugText += map[i].ToString() + ",";
-            }
-            Debug.Log(debugText);
         }
+        return ans;
+    }
+    bool MoveNumber(int number,int moveFrom,int moveto,int tolFrom,int i)
+    {
+        if (moveto < 0 || (moveto >= map.GetLength(i)))
+        {
+            return false;
+        }
+
+        if (map[moveto,tolFrom] == 2)
+        {
+            int velocity = moveto - moveFrom;
+
+            bool success = MoveNumber(2, moveto, moveto + velocity,tolFrom,i);
+
+            if (!success)
+            {
+                return false;
+            }
+        }
+        if (i == 1)
+        {
+            map[moveto, tolFrom] = number;
+            map[moveFrom, tolFrom] = 0;
+        }
+        if (i == 0)
+        {
+            map[moveto, tolFrom] = number;
+            map[moveFrom,moveto] = 0;
+        }
+        return true;
     }
 }
